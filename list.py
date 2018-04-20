57dc9a451843ddf8fba7faba6ba09df1a45060a3
James Q <jamesleroyq@gmail.com>
2018-04-20 22:00:26 +0100

    fixed bug, removed (+) in print and replaced with comma

--- list.py
+++ list.py
@@ -24,8 +24,8 @@ meanDataB = numpy.mean(b)
 meanDataC = numpy.mean(c)
 meanDataD = numpy.mean(d)
 
-print("The average petal length of the Iris-Setosa is: " + meanDataA)
-print("The average petal width of the Iris-Setosa is: " + meanDataB)
-print("The average sepal length of the Iris-Setosa is: " + meanDataC)
-print("The average of petal width of the Iris-Setosa is: " + meanDataD)
+print("The average petal length of the Iris-Setosa is: ",  meanDataA)
+print("The average petal width of the Iris-Setosa is: ",  meanDataB)
+print("The average sepal length of the Iris-Setosa is: ",  meanDataC)
+print("The average of petal width of the Iris-Setosa is: ", meanDataD)
 
