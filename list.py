cbf297f5733b638cad248b2f3736f8d1d93576c0
James Q <jamesleroyq@gmail.com>
2018-04-20 10:43:39 +0100

    calculate mean of each column of Iris-Setosa

--- list.py
+++ list.py
@@ -6,11 +6,19 @@ data = numpy.genfromtxt("datum/iris.csv", delimiter=",")
 
 # takes the first column of the whole dataset
 firstcol = data[:,0]
-
+seccol = data[:,1]
+thirdcol = data[:,2]
+fourthcol = data[:,3]
 #takes the first 50 of the first column
-x = firstcol[0:50]
+a = firstcol[0:50]
+b = seccol [0:50]
+c = thirdcol[0:50]
+d = fourthcol[0:50]
+
 
 #takes the mean of the first column
-meanData = numpy.mean(x)
-
-print(meanData)
+meanDataA = numpy.mean(a)
+meanDataB = numpy.mean(b)
+meanDataC = numpy.mean(c)
+meanDataD = numpy.mean(d)
+print(meanDataA, meanDataB, meanDataC, meanDataD)
