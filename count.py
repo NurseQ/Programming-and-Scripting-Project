af51329d3b1296ec63ecf704ae177f8138d78c30
James Q <jamesleroyq@gmail.com>
2018-04-21 01:03:27 +0100

    generate or count the number of rows

---
+++ count.py
@@ -0,0 +1,20 @@
+# James Quintin 20/04/18
+# Analysis of the Fishers Iris Flower Dataset
+
+# Count the number of rows, counting the first column
+
+import numpy
+
+# read the file 
+data = numpy.genfromtxt("datum/iris.csv", delimiter=",")
+
+# splits the array and takes the first column of the entire dataset
+firstcol = data[:,0]
+
+i = 0
+
+# loop to count the length of the first column
+while i < len(firstcol):
+    # print i and whatever i is in the list
+    print(i, firstcol[i]) 
+    i = i + 1
